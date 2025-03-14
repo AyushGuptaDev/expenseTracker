bff07d8ce2db263801ec303398547aa65a894b80
AyushGuptaDev <ayush11gupta2003@gmail.com>
2025-03-15 01:08:44 +0530

    login user

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,58 +1,102 @@
 import { User } from "../model/user.model.js";
 import {  uploadonCloudnery } from "../utils/cloudnery.js";
+
+
+
 const registerUser=async (req,res)=>{
 
-        
-        const {userName,email,password}=req.body;
+    const {userName,email,password}=req.body;
 
-        
-        if ([userName,email,password].some(field=>
-            field?.trim()===""))
-            {
-                return res.status(400).json({message:"all fields are required"});
-            }
-        
-        const existUser=await User.findOne(
-            {$or:[{userName},{email}]}
-        )  
-        
-        if(existUser){
-            return res.status(400).json({message:"userName or email already exists"});
+    
+    if ([userName,email,password].some(field=>
+        field?.trim()===""))
+        {
+            return res.status(400).json({message:"all fields are required"});
         }
+    
+    const existUser=await User.findOne(
+        {$or:[{userName:userName.toLowerCase()},{email:email.toLowerCase()}]}
+    )  
+    
+    if(existUser){
+        return res.status(400).json({message:"userName or email already exists"});
+    }
 
-        let coverImageUrl="";
+    let coverImageUrl="";
 
-        const filepath=req.file?.path;
+    const filepath=req.file?.path;
+    
+    if(filepath){
         
-        if(filepath){
-            
-            const coverImage=await uploadonCloudnery(filepath)
-            coverImageUrl=coverImage.secure_url;
+        const coverImage=await uploadonCloudnery(filepath)
+        coverImageUrl=coverImage.secure_url;
+    }
+
+    const user= await User.create({
+        userName : userName.toLowerCase(),
+        email:email.toLowerCase(),
+        password,
+        coverImage:coverImageUrl
+    });
+
+
+    const createduser=await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createduser){
+        return res.status(400).json({message:"something went wrong while creating user"});
+    }
+
+    return res.status(201).json(
+        {
+            message: "user created successfully",
+            user:createduser
         }
-
-        const user= await User.create({
-            userName : userName.toLowerCase(),
-            email,
-            password,
-            coverImage:coverImageUrl
-        });
-
-
-        const createduser=await User.findById(user._id).select(
-            "-password -refreshToken"
-        )
-
-        if(!createduser){
-            return res.status(400).json({message:"something went wrong while creating user"});
-        }
-
-        return res.status(201).json(
-            {
-                message: "user created successfully",
-                user:createduser
-            }
-        )
+    )
 }
 
 
-export {registerUser}
+const loginUser=async (req,res)=>{
+    const{userName,email,password}=req.body;
+
+    if((!email?.trim() && !userName?.trim()) || !password?.trim()) {
+        return res.status(400).json({ message: "Password and user details are required" });
+    }
+    
+
+    const user=await User.findOne(
+        {$or:[{userName:userName?.toLowerCase()},{email}]}
+    )
+    if(!user){
+        return res.status(404).json({message:"user not found"})
+    }
+    const checkpassword= await user.isPasswordCorrect(password);
+
+    if(!checkpassword){
+        return res.status(401).json({message:"incorrect password"});
+    }
+
+    const Accesstoken=user.genrateAcessToken;
+    const refreshtoken=user.genrateRefreshToken;
+    
+    user.refreshToken=refreshtoken;
+    user.save();
+
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+    const returnAbleUser=user.toObject();
+    delete returnAbleUser.password;
+    delete returnAbleUser.refreshToken;
+
+    return res.status(200).cookie("accesstoken",Accesstoken,options)
+    .cookie("refreshToken",refreshtoken,options)
+    .json({message:"user loggged in successfully",user:returnAbleUser});
+}
+
+
+export {registerUser,
+    loginUser,
+}
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { upload } from "../middleware/multer.middleware.js";
-import { registerUser } from "../controller/user.controller.js";
+import { registerUser, loginUser} from "../controller/user.controller.js";
 const router=Router();
 
+//router
 router.post("/signup",upload.single("coverImage"),registerUser);
+router.post("/login",loginUser);
 
 
 export default router;
